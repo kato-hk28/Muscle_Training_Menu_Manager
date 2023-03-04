8a596945f43caf3fbe6ab6cda9302baab50180e9
kato-hk28 <taihuijiateng870@gmail.com>
2023-03-04 16:22:47 +0900

    Initial Commit

---
+++ Muscle_Training_Menu_Manager/Muscle_Training_Menu_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Muscle_Training_Menu_ManagerApp.swift
+//  Muscle_Training_Menu_Manager
+//
+//  Created by Kato Taiki on 2023/03/04.
+//
+
+import SwiftUI
+
+@main
+struct Muscle_Training_Menu_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
